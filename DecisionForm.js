4399ba7be67822c90ecc74d659ca39b62a186306
Jeremy <j2005dbg@gmail.com>
2025-11-06 15:31:04 -0800

    Integrate DecisionForm into DecisionsPage

--- DecisionForm.js
+++ DecisionForm.js
@@ -1,1 +1,10 @@
-const [log, setLog] = useState([]);
+import DecisionForm from "../components/DecisionForm";
+
+export default function DecisionsPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Personal Decision Tool</h1>
+      <DecisionForm />
+    </div>
+  );
+}
